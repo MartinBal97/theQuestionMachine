80bbeae067da38f144983148781329976b16d8ed
deiner00077 <dg00077@hotmail.com>
2023-01-17 17:03:50 +0100

    nuevo adelanto

---
+++ js/quiz.js
@@ -0,0 +1,8 @@
+
+let preguntas = fetch("https://the-trivia-api.com/api/questions")
+console.log(preguntas);
+    // .then(response => response.json())
+    //  .then(data => data)
+    //  .then(data => console.log(data))
+
+   
