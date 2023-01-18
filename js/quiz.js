14ee8c89985ef6ad621ca2873da86564656757fb
DaviBataglia20 <davi.bataglia14@gmail.com>
2023-01-18 16:49:25 +0100

    se realiza cambios de orden de respuestas

--- js/quiz.js
+++ js/quiz.js
@@ -1,14 +1,56 @@
-const preguntas = document.querySelectorAll(".cartaPregunta>p")
+const divNuevas = document.querySelectorAll(".carta")
+const arrayDeLasDiv = []
+divNuevas.forEach(div =>{
+    arrayDeLasDiv.push(div)
+})
+const divpai = document.querySelector(".cartaPregunta")
+divNuevas.forEach(div =>{
+    divpai.removeChild(div)
+})
+console.log(arrayDeLasDiv)
+arrayDeLasDiv.sort(() => Math.random() - 0.5);
+console.log(arrayDeLasDiv);
+arrayDeLasDiv.forEach(div =>{
+    divpai.appendChild(div)
+})
+
+
+
+
+
+
+
+const preguntas = document.querySelectorAll(".cartaPregunta>.carta>p")
+let cont = 0
 console.log(preguntas)
 
-/* Funcion que cambia la respuesta correcta a verde y roja a las otras */
+arrayNuevo =  [];
+preguntas.forEach(pregunta =>{
+    arrayNuevo.push(pregunta)
+})
+arrayNuevo.sort(() => Math.random() - 0.5);
+console.log(arrayNuevo);
 
-/* for(i=0 ; i < cuestiones.length ; i++){
+preguntas.forEach((pregunta,i) => {
+
+    pregunta.innerHTML=`${arrayNuevo[i].innerText}`
+    
+})
 
 
-} */
+
+
+
+
+
+
+
+
+
+
+
+
 const botonNext = document.querySelector("#proximo")
-console.log(botonNext)
 botonNext.addEventListener("click",()=>{
     preguntas.forEach(pregunta =>{
         if(pregunta.getAttribute("value") === "correcta"){
@@ -29,4 +71,13 @@ preguntas.forEach(pregunta =>{
         })
     })
 })
+const questionsAPI = fetch("https://the-trivia-api.com/api/questions")
+const containerPrincipal = document.querySelector(".containerPrincipal")
+const preguntos = [];
+questionsAPI
+    .then(res => res.json())
+    .then(data =>  data.forEach(e => {
+        preguntos.push(e)
+    }))
 
+console.log(preguntos);
