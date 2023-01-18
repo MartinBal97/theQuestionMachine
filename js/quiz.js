03abc9d220f27853d60260a88a47f9fe234df11d
Martin Balverde <tinchoabalverde@gmail.com>
2023-01-18 16:58:39 +0100

    Alfredo fan de la mandarina

--- js/quiz.js
+++ js/quiz.js
@@ -5,13 +5,21 @@ const preguntas = []
 questionsAPI
     .then(res => res.json())
     .then(data =>  data.forEach(e => {
-        preguntas.push(e)
+
+        containerPrincipal.innerHTML += `
+        <div class="containerPregunta">
+            <h3>${e.question}</h3>
+            <div class="preguntas">
+                <div class="pregunta"><input type="radio" name="responses" value="correct">${e.correctAnswer}</label></div>
+                <div class="pregunta"><input type="radio" name="responses" value="incorrect">${e.incorrectAnswers[0]}</label></div></div>
+                <div class="pregunta"><input type="radio" name="responses" value="incorrect">${e.incorrectAnswers[1]}</label></div></div>
+                <div class="pregunta"><input type="radio" name="responses" value="incorrect">${e.incorrectAnswers[2]}</label></div></div>
+            </div>
+            <button onclick="nextQuestion(event)" class="btn-eliminar">Eliminar</button>
+        </div>`
     }))
 
-console.log(preguntas);
-
-preguntas.forEach((pregunta) => {
-    containerPrincipal.innerHTML = `
-
-    `
-})
+    function nextQuestion(event) {
+        
+    }
+    
