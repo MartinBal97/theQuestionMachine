0e862b89923b4342fefde23eeec9a1cd67d8e38e
deiner00077 <dg00077@hotmail.com>
2023-01-19 16:20:50 +0100

    avance css

--- js/quiz.js
+++ js/quiz.js
@@ -1,8 +1,21 @@
 
-let preguntas = fetch("https://the-trivia-api.com/api/questions")
-console.log(preguntas);
-    // .then(response => response.json())
-    //  .then(data => data)
-    //  .then(data => console.log(data))
+// 
+//  </div>
+//     <div class="preguntas">
+//          <div class="pregunta1"><input type="radio"><label>asdfafgsdadfgasdgf</label></div>
+//          <div class="pregunta2"><input type="radio"><label>ghjdfjghdfjddf</label></div>
+//          <div class="pregunta3"><input type="radio"><label>fsghgfdhsghs</label></div>
+//          <div class="pregunta4"><input type="radio"><label>shfshgfdhsfdgsdfgh</label></div>
+//  </div>
+//          <button>next question</button>  <button>result</button>
 
-   
+// `
+// contenedor.append.div
+// }))
+//  
+
+const pregunta = fetch("https://the-trivia-api.com/api/questions")
+    .then(response => response.json())
+    .then(data => data)
+
+localStorage.setItem('data', JSON.stringify(data))
