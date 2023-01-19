fa891e28e0a0ec6e46b9f9a45c012b38d941c656
Martin Balverde <tinchoabalverde@gmail.com>
2023-01-19 16:05:26 +0100

    se llama a la api cada vez que se clickea next question

--- js/quiz.js
+++ js/quiz.js
@@ -1,25 +1,35 @@
-const questionsAPI = fetch("https://the-trivia-api.com/api/questions")
-const containerPrincipal = document.querySelector(".containerPrincipal")
-const preguntas = []
+const containerPrincipal = document.querySelector(".containerPrincipal");
+const nextQuestion = document.querySelector(".nextQuestion");
 
-questionsAPI
-    .then(res => res.json())
-    .then(data =>  data.forEach(e => {
 
-        containerPrincipal.innerHTML += `
+let cont = 0;
+nextQuestion.addEventListener("click", () => {
+    cont++;
+    fetch("https://the-trivia-api.com/api/questions?limit=1")
+        .then((res) => res.json())
+        .then((e) => {
+
+
+        containerPrincipal.innerHTML = `
         <div class="containerPregunta">
-            <h3>${e.question}</h3>
+            <h3>${e[0].question}</h3>
             <div class="preguntas">
-                <div class="pregunta"><input type="radio" name="responses" value="correct">${e.correctAnswer}</label></div>
-                <div class="pregunta"><input type="radio" name="responses" value="incorrect">${e.incorrectAnswers[0]}</label></div></div>
-                <div class="pregunta"><input type="radio" name="responses" value="incorrect">${e.incorrectAnswers[1]}</label></div></div>
-                <div class="pregunta"><input type="radio" name="responses" value="incorrect">${e.incorrectAnswers[2]}</label></div></div>
+                <div id="1" class="pregunta"><input type="radio" name="responses${cont}" value="correcta">${e[0].correctAnswer}</label></div>
+                <div id="2" class="pregunta"><input type="radio" name="responses${cont}" value="incorrecta">${e[0].incorrectAnswers[0]}</label></div></div>
+                <div id="3" class="pregunta"><input type="radio" name="responses${cont}" value="incorrecta">${e[0].incorrectAnswers[1]}</label></div></div>
+                <div id="4" class="pregunta"><input type="radio" name="responses${cont}" value="incorrecta">${e[0].incorrectAnswers[2]}</label></div></div>
             </div>
-            <button onclick="nextQuestion(event)" class="btn-eliminar">Eliminar</button>
-        </div>`
-    }))
+        </div>`;
 
-    function nextQuestion(event) {
-        
-    }
-    
+            const divNuevas = document.querySelectorAll(".pregunta") /* SELECCIONANDO LAS DIV QUE ESTAN AL PRINCIPIO EN EL HTML */
+            const arrayDeLasPreguntas = []
+            divNuevas.forEach(div => {
+                arrayDeLasPreguntas.push(div) /* UN FOR EACH QUE PUSHEA LAS DIV PARA EL ARRAY "arrayDeLasDiv" */
+            })
+
+            
+            console.log(arrayDeLasPreguntas);
+
+
+        });
+});
