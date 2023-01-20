a7919d87e1afcfdd94bc559a38ea520911b097d3
DaviBataglia20 <davi.bataglia14@gmail.com>
2023-01-20 10:59:12 +0100

    logica va mal

--- js/quiz.js
+++ js/quiz.js
@@ -27,53 +27,79 @@ const divpai = document.querySelector(".cartaPregunta") /* AQUI SE SELECCIONA LA
         divpai.removeChild(div) /* ESTE FOR EACH LO QUE HACES ES REMOVER LAS DIVS DEFAULT PARA LUEGO REAÑADIR CON LAS NUEVAS */
     })
     arrayDeLasDiv.sort(() => Math.random() - 0.5); /* ESTE EL LA PROPIEDAD DEL ARRAY QUE HACE LA POSICION DEL ELEMENTO(LA DIV PREGUNTA) DEL ARRAY QUEDAR ALEATORIO */
-    console.log(arrayDeLasDiv);
     arrayDeLasDiv.forEach(div =>{
+        div.setAttribute("name","activado")
         divpai.appendChild(div) /* AQUI AÑADE AL CONTENEDOR DE LAS DIV YA CON LOS CAMBIOS EN ALEATORIO*/
     })
+let cont = 1;
+/* FUNCION QUE ACTIVA EL BOTON DEL NEXT */
+const activarNext = elBotonNext =>{
+    const elBotonPasar = document.querySelector(".nextQuestion")
+    elBotonPasar.addEventListener("click",()=>{
+        cont++
+        if(cont != 11){
+        let objeto = arrayLocal[arrayLocal.length-cont];
+        cuestionCorrecta.innerHTML = `<p value ="correcta">${objeto.correctAnswer}</p>` /* AQUI CAMBIA EL HTML PARA CUESTION CORRECTA */
+        inco1.innerHTML =`<p value ="incorrecta">${objeto.incorrectAnswers[0]}`
+        inco2.innerHTML =`<p value ="incorrecta">${objeto.incorrectAnswers[1]}`
+        inco3.innerHTML =`<p value ="incorrecta">${objeto.incorrectAnswers[2]}`
+        let functionCalled = false;
+        console.log(cont)
+        }else{
+            console.log("llegue a 10")
+        }
+    })
+}
 
-const arrayLocal =  JSON.parse(localStorage.getItem("array")) /* AQUI SE OBTIENE EL ARRAY PREGUNTAS DEL LOCAL STORAGE */
+let arrayLocal =  JSON.parse(localStorage.getItem("array")) /* AQUI SE OBTIENE EL ARRAY PREGUNTAS DEL LOCAL STORAGE */
 
 
 /* ITERACIONES POR EL ARRAY DE LAS  PREGUNTAS */
-const objeto = arrayLocal[arrayLocal.length-1] /* ESTO SERIA LA SELECCION DEL INDEX PARA ITERAR POR UN UNICO OBJETO,LUEGO CON EL BOTON NEXT ESTE INDEX VA A CAMBIAR */
+let objeto = arrayLocal[arrayLocal.length- 1] /* ESTO SERIA LA SELECCION DEL INDEX PARA ITERAR POR UN UNICO OBJETO,LUEGO CON EL BOTON NEXT ESTE INDEX VA A CAMBIAR */
+
+
 /* AQUI SE CREA UNA VARIABLE QUE SELECCIONA LA DIV CON ID#1 PARA QUE SEA LA CUESTION CORRECTA */
-const cuestionCorrecta = arrayDeLasDiv.find(element => element.id === "1")
+let cuestionCorrecta = arrayDeLasDiv.find(element => element.id === "1")
 
 cuestionCorrecta.innerHTML = `<p value ="correcta">${objeto.correctAnswer}</p>` /* AQUI CAMBIA EL HTML PARA CUESTION CORRECTA */
 
 /* AQUI SE CREA LAS VARIABLES DE LAS INCORRECTAS */
-const inco1 = arrayDeLasDiv.find(element => element.id === "2")
-const inco2 = arrayDeLasDiv.find(element => element.id === "3")
-const inco3 = arrayDeLasDiv.find(element => element.id === "4")
+let inco1 = arrayDeLasDiv.find(element => element.id === "2")
+let inco2 = arrayDeLasDiv.find(element => element.id === "3")
+let inco3 = arrayDeLasDiv.find(element => element.id === "4")
 
 inco1.innerHTML =`<p value ="incorrecta">${objeto.incorrectAnswers[0]}`
 inco2.innerHTML =`<p value ="incorrecta">${objeto.incorrectAnswers[1]}`
 inco3.innerHTML =`<p value ="incorrecta">${objeto.incorrectAnswers[2]}`
 
-/* ESTO ESTA COMENTADO PERO SON LOS BOTONES DE NEXT*/
-/* const botonNext = document.querySelector("#proximo")
-botonNext.addEventListener("click",()=>{
-    preguntaas.forEach(pregunta =>{
-        if(pregunta.getAttribute("value") === "correcta"){
-            pregunta.style.color = "hsl(177, 72%, 37%)";
-        }else if(pregunta.getAttribute("value") === "incorrecta"){
-            pregunta.style.color = "#FF0000";
-        }
-})
-}) */
+let functionCalled = false;
+
+
 
 /* FOR EACH QUE CAMBIA EL COLOR DE LAS CARTAS CUANDO UNA SE DA CLICK */
 arrayDeLasDiv.forEach(div =>{
-    div.addEventListener("click",()=>{
-        arrayDeLasDiv.forEach(div =>{
+    div.addEventListener("click",()=>{ 
         const cambiao = div.childNodes[0];
+        if(cambiao.getAttribute("value") == "correcta"){
+            console.log("respuesta correcta")
+        }else if (cambiao.getAttribute("value") == "incorrecta"){
+            console.log("respuesta incorrecta")
+        }
+        arrayDeLasDiv.forEach(div =>{
+            const cambiao = div.childNodes[0];
         if(cambiao.getAttribute("value") == "correcta"){
             cambiao.style.color = "	#7CFC00"
         }else if (cambiao.getAttribute("value") == "incorrecta"){
             cambiao.style.color ="#FF0000"
         }
+        if(!functionCalled){
+            activarNext();
+        
+            functionCalled = true;
+        }
+       
     })
     })
 })
+
 
